7d3dcaf6ea8bb6ee57b1400208867590f15d1b09
Euichang Park <euichangpark@gmail.com>
2022-01-17 09:38:15 -0500

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,11 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+const firstElement = languages[0];
+const thirdElement = languages[2];
+const length = languages.length;
+const lastElement = languages[length - 1];
+
+console.log(languages);
+console.log('the first element of the array is ' + firstElement);
+console.log('the 3rd element of the array is ' + thirdElement);
+console.log('The length of the array is ' + length);
+console.log('The last element of the array is ' + lastElement);
